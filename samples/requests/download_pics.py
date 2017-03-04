05e3e42b35d67e0358c8ead5b5a2ac01001ffb99
XiongNeng <yidao620@gmail.com>
2017-03-04 17:08:14 +0800

    七牛图片-->腾讯云的迁移，文件名比较，防止漏传

--- samples/requests/download_pics.py
+++ samples/requests/download_pics.py
@@ -10,10 +10,10 @@ import requests
 import re
 import os
 
-def down_allpic(crush_file):
+def down_allpic(html_file):
     urls = []
     id_pattern = re.compile(r' data-clipboard-text="(.*?)"', re.MULTILINE)
-    with open(crush_file, encoding='utf-8') as f:
+    with open(html_file, encoding='utf-8') as f:
         content = f.read()
     for m in id_pattern.finditer(content):
         urls.append(m.group(1))
@@ -33,5 +33,34 @@ def down_allpic(crush_file):
     return urls
 
 
+def compare_diff(qiniuhtml, coshtml):
+    urls1 = set()
+    id_pattern = re.compile(r' data-clipboard-text="(.*?)"', re.MULTILINE)
+    with open(qiniuhtml, encoding='utf-8') as f:
+        content = f.read()
+    for m in id_pattern.finditer(content):
+        url = m.group(1)
+        if (url.endswith('.zip') or url.endswith('/')):
+            continue
+        urls1.add(os.path.split(url)[1])
+
+    urls2 = set()
+    id_pattern2 = re.compile(r' filename="(.*?)"', re.MULTILINE)
+    with open(coshtml, encoding='utf-8') as f:
+        content = f.read()
+    for m in id_pattern2.finditer(content):
+        url = m.group(1)
+        if (url.endswith('.zip') or url.endswith('/')):
+            continue
+        urls2.add(url)
+
+    print(urls1.difference(urls2))
+    print(urls2.difference(urls1))
+    print(urls1.symmetric_difference(urls2))
+
+
 if __name__ == '__main__':
-    down_allpic(r'D:/work/projects/gitprojects/core-python/zzz.txt')
+    # down_allpic(r'D:/work/projects/gitprojects/core-python/zzz.txt')
+    compare_diff(r'D:/work/projects/gitprojects/core-python/zzz.txt',
+                 r'D:/work/projects/gitprojects/core-python/zzz1.txt')
+
