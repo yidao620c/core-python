db72688b12356fbd2ba5764bdaf94952c639f8a3
yidao620 <yidao620@gmail.com>
2017-06-23 15:34:08 +0800

    mapper全批量生成

--- samples/excel/generate_javabean.py
+++ samples/excel/generate_javabean.py
@@ -2,7 +2,15 @@
 # -*- encoding: utf-8 -*-
 """
 Topic: 通过一个schema.sql来生成标准表结构的javabean
-使用方法：python generate_javabean.py beans_dir package_name schema_name
+
+使用方法：python generate_javabean.py src_base_dir domain_package mapper_package xml_dir schema_name
+
+参数：
+1. src_base_dir     源码基础路径：E:\projects\tobacco-back1\src\main\java
+2. domain_package   domain类的包名：com.enzhico.epay.domain.base
+3. mapper_package   mapper类的包名：com.enzhico.epay.mapper
+4. xml_dir          mapper xml绝对路径：E:\projects\tobacco-back1\src\main\resources\mapper
+5. schema_name      schema sql文件的绝对路径：E:\projects\tobacco-back1\src\main\resources\schema.sql
 """
 import sys
 import os
@@ -13,6 +21,7 @@ _package_location_
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -32,14 +41,19 @@ public class BaseDomain implements Serializable {
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
     /**
      * 创建时间
      */
+    @Column(name = "created_time")
     private Date createdTime;
+
     /**
      * 更新时间
      */
+    @Column(name = "updated_time")
     private Date updatedTime;
 
     public String toString() {
@@ -109,68 +123,76 @@ BASE_FIELS = {'id', 'updated_time', 'created_time'}
 # MySQL type to java type
 MYSQL_TYPE_MAP = {
     'BIT(1)': ('Boolean',)
-    ,'BIT': ('byte[]',)
-    ,'TINYINT': ('Integer',)
-    ,'BOOLEAN': ('Boolean',)
-    ,'BOOL': ('Boolean',)
-    ,'SMALLINT': ('Integer',)
-    ,'MEDIUMINT': ('Integer',)
-    ,'INT': ('Integer',)
-    ,'INTEGER': ('Integer',)
-    ,'BIGINT': ('Long',)
-    ,'FLOAT': ('Float',)
-    ,'DOUBLE': ('Double',)
-    ,'DECIMAL': ('BigDecimal', 'java.math.BigDecimal')
-    ,'DATE': ('Date', 'java.util.Date')
-    ,'DATETIME': ('Date', 'java.util.Date')
-    ,'TIMESTAMP': ('Date', 'java.util.Date')
-    ,'TIME': ('Date', 'java.util.Date')
-    ,'CHAR': ('String ',)
-    ,'VARCHAR': ('String',)
-    ,'BINARY': ('byte[]',)
-    ,'VARBINARY': ('byte[]',)
-    ,'TINYBLOB': ('byte[]',)
-    ,'TINYTEXT': ('String',)
-    ,'BLOB': ('byte[]',)
-    ,'TEXT': ('String',)
-    ,'MEDIUMBLOB': ('byte[]',)
-    ,'MEDIUMTEXT': ('String',)
-    ,'LONGBLOB': ('byte[]',)
-    ,'LONGTEXT': ('String',)
-    ,'ENUM': ('String',)
-    ,'SET': ('String',)
-    ,'bit(1)': ('Boolean',)
-    ,'bit': ('byte[]',)
-    ,'tinyint': ('Integer',)
-    ,'boolean': ('Boolean',)
-    ,'bool': ('Boolean',)
-    ,'smallint': ('Integer',)
-    ,'mediumint': ('Integer',)
-    ,'int': ('Integer',)
-    ,'integer': ('Integer',)
-    ,'bigint': ('Long',)
-    ,'float': ('Float',)
-    ,'double': ('Double',)
-    ,'decimal': ('BigDecimal', 'java.math.BigDecimal')
-    ,'date': ('Date', 'java.util.Date')
-    ,'datetime': ('Date', 'java.util.Date')
-    ,'timestamp': ('Date', 'java.util.Date')
-    ,'time': ('Date', 'java.util.Date')
-    ,'char': ('String ',)
-    ,'varchar': ('String',)
-    ,'binary': ('byte[]',)
-    ,'varbinary': ('byte[]',)
-    ,'tinyblob': ('byte[]',)
-    ,'tinytext': ('String',)
-    ,'blob': ('byte[]',)
-    ,'text': ('String',)
-    ,'mediumblob': ('byte[]',)
-    ,'mediumtext': ('String',)
-    ,'longblob': ('byte[]',)
-    ,'longtext': ('String',)
-    ,'enum': ('String',)
-    ,'set': ('String',)
+    , 'BIT': ('byte[]',)
+    , 'TINYINT': ('Integer',)
+    , 'BOOLEAN': ('Boolean',)
+    , 'BOOL': ('Boolean',)
+    , 'SMALLINT': ('Integer',)
+    , 'MEDIUMINT': ('Integer',)
+    , 'INT': ('Integer',)
+    , 'INTEGER': ('Integer',)
+    , 'BIGINT': ('Long',)
+    , 'FLOAT': ('Float',)
+    , 'DOUBLE': ('Double',)
+    , 'DECIMAL': ('BigDecimal', 'java.math.BigDecimal')
+    , 'DATE': ('Date', 'java.util.Date')
+    , 'DATETIME': ('Date', 'java.util.Date')
+    , 'TIMESTAMP': ('Date', 'java.util.Date')
+    , 'TIME': ('Date', 'java.util.Date')
+    , 'CHAR': ('String ',)
+    , 'VARCHAR': ('String',)
+    , 'BINARY': ('byte[]',)
+    , 'VARBINARY': ('byte[]',)
+    , 'TINYBLOB': ('byte[]',)
+    , 'TINYTEXT': ('String',)
+    , 'BLOB': ('byte[]',)
+    , 'TEXT': ('String',)
+    , 'MEDIUMBLOB': ('byte[]',)
+    , 'MEDIUMTEXT': ('String',)
+    , 'LONGBLOB': ('byte[]',)
+    , 'LONGTEXT': ('String',)
+    , 'ENUM': ('String',)
+    , 'SET': ('String',)
+    , 'bit(1)': ('Boolean',)
+    , 'bit': ('byte[]',)
+    , 'tinyint': ('Integer',)
+    , 'boolean': ('Boolean',)
+    , 'bool': ('Boolean',)
+    , 'smallint': ('Integer',)
+    , 'mediumint': ('Integer',)
+    , 'int': ('Integer',)
+    , 'integer': ('Integer',)
+    , 'bigint': ('Long',)
+    , 'float': ('Float',)
+    , 'double': ('Double',)
+    , 'decimal': ('BigDecimal', 'java.math.BigDecimal')
+    , 'date': ('Date', 'java.util.Date')
+    , 'datetime': ('Date', 'java.util.Date')
+    , 'timestamp': ('Date', 'java.util.Date')
+    , 'time': ('Date', 'java.util.Date')
+    , 'char': ('String ',)
+    , 'varchar': ('String',)
+    , 'binary': ('byte[]',)
+    , 'varbinary': ('byte[]',)
+    , 'tinyblob': ('byte[]',)
+    , 'tinytext': ('String',)
+    , 'blob': ('byte[]',)
+    , 'text': ('String',)
+    , 'mediumblob': ('byte[]',)
+    , 'mediumtext': ('String',)
+    , 'longblob': ('byte[]',)
+    , 'longtext': ('String',)
+    , 'enum': ('String',)
+    , 'set': ('String',)
 }
+
+MAPPER_XML = """<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN"
+        "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+<mapper namespace="{}.{}Mapper">
+</mapper>
+"""
+
 
 def camel_to_underline(camel_format):
     """
@@ -222,21 +244,39 @@ def load_schema(filename):
     return result
 
 
-def write_beans(beans_dir, package_name, schema_name):
+def write_beans(src_base_dir, domain_package, mapper_package, xml_dir, schema_name):
+
+    beans_dir = os.path.join(src_base_dir, domain_package.replace('.', os.sep))
+    mapper_dir = os.path.join(src_base_dir, mapper_package.replace('.', os.sep))
+    xml_dir = os.path.join(src_base_dir, xml_dir)
+
+    if not os.path.exists(beans_dir):
+        os.makedirs(beans_dir)
+    if not os.path.exists(mapper_dir):
+        os.makedirs(mapper_dir)
+    if not os.path.exists(xml_dir):
+        os.makedirs(xml_dir)
+
+    domain_package_with_semicolon = domain_package + ";"
+    mapper_package_with_semicolon = mapper_package + ";"
+
     # 今日格式化字符串
     today_str = ' * @since {}'.format(datetime.datetime.now().strftime('%Y/%m/%d'))
     # 先写BaseDomain.java
     with open(os.path.join(beans_dir, 'BaseDomain.java'), mode='w', encoding='utf-8') as jf:
-        jf.write(BASE_DOMAIN.replace('_package_location_', 'package ' + package_name))
+        jf.write(BASE_DOMAIN.replace('_package_location_', 'package ' + domain_package_with_semicolon))
+
     table_data = load_schema(schema_name)
+    # 然后开始对每个表生成一个Domain类
     for table in table_data:
         table_name_comment = table[0]
         table_name_real = table[1]
         class_name = underline_to_camel(table_name_real[2:])
         lines = []
-        lines.append('package ' + package_name)
+        lines.append('package ' + domain_package_with_semicolon)
         lines.append('\n')
         lines.append('import javax.persistence.Table;')
+        lines.append('import javax.persistence.Column;')
         lines.append('\n')
         lines.append('/**')
         lines.append(' * ' + table_name_comment)
@@ -271,6 +311,7 @@ def write_beans(beans_dir, package_name, schema_name):
             lines_fields.append('    /**')
             lines_fields.append('     * {}'.format(column_comment))
             lines_fields.append('     */')
+            lines_fields.append('    @Column(name = "{}")'.format(column_name))
             lines_fields.append('    private {} {};'.format(java_type, field_name))
             if import_str:
                 other_import.add(import_str)
@@ -311,13 +352,52 @@ def write_beans(beans_dir, package_name, schema_name):
         with open(os.path.join(beans_dir, java_file), mode='w', encoding='utf-8') as jf:
             jf.writelines(lines)
 
+    # 然后开始对每个表生成一个Mapper类和一个xml配置文件
+    for table in table_data:
+        table_name_comment = table[0]
+        table_name_real = table[1]
+        class_name = underline_to_camel(table_name_real[2:])
+        lines = []
+        lines.append('package ' + mapper_package_with_semicolon)
+        lines.append('\n')
+        lines.append('import {}.{};'.format(domain_package, class_name))
+        lines.append('import tk.mybatis.mapper.common.Mapper;')
+        lines.append('\n')
+        lines.append('/**')
+        lines.append(' * ' + table_name_comment + " Mapper")
+        lines.append(' *')
+        lines.append(' * @author XiongNeng')
+        lines.append(' * @version 1.0')
+        lines.append(today_str)
+        lines.append(' */')
+        lines.append('public interface {0}Mapper extends Mapper<{0}> {{'.format(class_name))
+        lines.append('\n')
+        lines.append('}')
+
+        lines = [line + "\n" if line != '\n' else line for line in lines]
+        # 开始写mapper源文件
+        with open(os.path.join(mapper_dir, '{}Mapper.java'.format(class_name)), mode='w', encoding='utf-8') as jf:
+            jf.writelines(lines)
+
+        # 开始写mapper的xml配置文件
+        with open(os.path.join(xml_dir, '{}Mapper.xml'.format(class_name)), mode='w', encoding='utf-8') as jf:
+            jf.write(MAPPER_XML.format(mapper_package, class_name))
+    print('successful...')
+    pass
 
 if __name__ == '__main__':
-    # print(camel_to_underline("CompanyServiceImpl"))
-    # print(underline_to_camel("company_service_impl", True))
-    beans_dir = r'D:\work\zzz'
-    package_name = r'com.cmback.tobacco.domain;'
-    schema_name = r'D:\work\freedom\tobacco\src\main\resources\sql\schema.sql'
-    write_beans(beans_dir, package_name, schema_name)
-    # write_beans(sys.argv[1], sys.argv[2], sys.argv[3])
-    pass
+    if len(sys.argv) > 5:
+        src_base_dir = sys.argv[1]
+        domain_package = sys.argv[2]
+        mapper_package = sys.argv[3]
+        xml_dir = sys.argv[4]
+        schema_name = sys.argv[5]
+        write_beans(src_base_dir, domain_package, mapper_package, xml_dir, schema_name)
+    else:
+        src_base_dir = r'E:\projects\tobacco-back1\src\main\java'
+        domain_package = 'com.enzhico.epay.domain'
+        mapper_package = 'com.enzhico.epay.mapper.base'
+        xml_dir = r'E:\projects\tobacco-back1\src\main\resources\mapper'
+        schema_name = r'E:\projects\tobacco1\src\main\resources\sql\schema.sql'
+    write_beans(src_base_dir, domain_package, mapper_package, xml_dir, schema_name)
+
