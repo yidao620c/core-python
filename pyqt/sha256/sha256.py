4b2bcfc28b425137001e3763a5dd1c2da07a5532
xiongneng <yidao620@gmail.com>
2022-01-24 01:05:23 +0800

    update pyqt5

--- pyqt/sha256/sha256.py
+++ pyqt/sha256/sha256.py
@@ -9,8 +9,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        # 实例化线程对象
-        self.work = WorkThread()
+        # 定义button的槽函数
         self.pushButton.clicked.connect(self.open_dir)
         self.pushButton_2.clicked.connect(self.execute)
 
@@ -18,48 +17,55 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         file_path = QFileDialog.getExistingDirectory(self, "选取指定文件夹", "C:/")
         if file_path:
             self.lineEdit.setText(file_path)
+            self.plainTextEdit.appendPlainText(f"11111111111111")
+            self.plainTextEdit.appendPlainText(f"22222222222222")
 
     def execute(self):
         if self.lineEdit.text():
-            # 启动线程
-            self.work.start()
+            # 实例化线程对象
+            choose_dir = self.lineEdit.text()
+            work = WorkThread(choose_dir=choose_dir)
             # 线程自定义信号连接的槽函数
-            self.work.started.connect(self.start)
-            self.work.trigger.connect(self.display)
-            self.work.finished.connect(self.end)
+            work.started.connect(self.start)
+            work.trigger.connect(self.display)
+            work.finished.connect(self.end)
+            # 启动线程
+            work.start()
         else:
             QMessageBox.warning(self, "警告对话框", "请先选择文件夹")
 
     def start(self):
         # 线程开始的处理
-        self.plainTextEdit.addItem(f">>>>>>>>>>>>all start<<<<<<<<<<<<<<")
+        self.plainTextEdit.appendPlainText(f">>>>>>>>>>>>all start<<<<<<<<<<<<<<")
+        print("11111111111111111")
 
     def display(self, params):
         # 由于自定义信号时自动传递一个参数对象
-        self.plainTextEdit.addItem(f"{params['prefix']}:{params['line']}")
+        self.plainTextEdit.appendPlainText(f"{params['prefix']}:{params['line']}")
 
     def end(self):
         # 线程结束的处理
-        self.plainTextEdit.addItem(f">>>>>>>>>>>>all finished<<<<<<<<<<<<<<")
+        self.plainTextEdit.appendPlainText(f">>>>>>>>>>>>all finished<<<<<<<<<<<<<<")
+        print("2222222222222")
 
 
 class WorkThread(QThread):
     # 自定义信号对象。参数dict就代表这个信号可以传一个dict对象
     trigger = pyqtSignal(dict)
 
-    def __int__(self):
+    def __int__(self, parent=None, _choose_dir=None):
         # 初始化函数
         super(WorkThread, self).__init__()
+        self.choose_dir = _choose_dir
 
     def run(self):
         # 重写线程执行的run函数
         # 触发自定义信号
-        choosed_dir = self.lineEdit.text()
         for i in range(20):
-            self.trigger.emit({"prefix": "start", "line": f"{str(i)} - {choosed_dir}"})
+            self.trigger.emit({"prefix": "start", "line": f"{str(i)} - {self.choose_dir}"})
             time.sleep(1)
             # 通过自定义信号把待显示的字符串传递给槽函数
-            self.trigger.emit({"prefix": "end", "line": f"{str(i)} - {choosed_dir}"})
+            self.trigger.emit({"prefix": "end", "line": f"{str(i)} - {self.choose_dir}"})
 
 
 def main():
