12607f464bd22cd75ec1a7b41f6ecb8f9866296b
xiongneng <yidao620@gmail.com>
2022-01-24 21:19:47 +0800

    完成了一个自动生成json文件chk校验码的小程序。

--- pyqt/sha256/sha256.py
+++ pyqt/sha256/sha256.py
@@ -1,5 +1,8 @@
 import sys
+import os
+import hashlib
 import time
+import cgitb
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog, QFileDialog
 from .sha256_ui import *
@@ -17,37 +20,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         file_path = QFileDialog.getExistingDirectory(self, "选取指定文件夹", "C:/")
         if file_path:
             self.lineEdit.setText(file_path)
-            self.plainTextEdit.appendPlainText(f"11111111111111")
-            self.plainTextEdit.appendPlainText(f"22222222222222")
 
     def execute(self):
         if self.lineEdit.text():
-            # 实例化线程对象
-            work = WorkThread(choose_dir=self.lineEdit.text())
+            # 实例化线程对象，注意之类一定要设置成成员变量self，因为生命周期原因
+            self.work = WorkThread(choose_dir=self.lineEdit.text())
             # 线程自定义信号连接的槽函数
-            work.started.connect(self.start)
-            work.trigger.connect(self.display)
-            work.finished.connect(self.end)
+            self.work.started.connect(self.start)
+            self.work.trigger.connect(self.display)
+            self.work.warn_signal.connect(self.warn)
+            self.work.finished.connect(self.end)
             # 启动线程
-            work.start()
+            self.work.start()
         else:
             QMessageBox.warning(self, "警告对话框", "请先选择文件夹")
 
     def start(self):
         # 线程开始的处理
-        self.plainTextEdit.appendPlainText(f">>>>>>>>>>>>all start<<<<<<<<<<<<<<")
+        self.plainTextEdit.clear()
+        self.plainTextEdit.appendPlainText(f">>>>>all start<<<<<")
 
     def display(self, params):
         # 由于自定义信号时自动传递一个参数对象
         self.plainTextEdit.appendPlainText(f"{params['prefix']}:{params['line']}")
 
+    def warn(self):
+        # 显示警告
+        QMessageBox.warning(self, "警告对话框", "该文件夹不存在，请重新选择!")
+
     def end(self):
         # 线程结束的处理
-        self.plainTextEdit.appendPlainText(f">>>>>>>>>>>>all finished<<<<<<<<<<<<<<")
+        self.plainTextEdit.appendPlainText(f">>>>>all finished<<<<<<")
 
 
 class WorkThread(QThread):
-    # 自定义信号对象。参数dict就代表这个信号可以传一个dict对象
+    # 自定义信号对象，无参数
+    warn_signal = pyqtSignal()
+    # 自定义信号对象，参数dict就代表这个信号可以传一个dict对象
     trigger = pyqtSignal(dict)
 
     def __init__(self, choose_dir: str = None):
@@ -57,14 +66,25 @@ class WorkThread(QThread):
 
     def run(self):
         # 重写线程执行的run函数，触发自定义信号
-        for i in range(20):
-            self.trigger.emit({"prefix": "start", "line": f"{str(i)} - {self.choose_dir}"})
-            time.sleep(1)
-            # 通过自定义信号把待显示的字符串传递给槽函数
-            self.trigger.emit({"prefix": "end", "line": f"{str(i)} - {self.choose_dir}"})
+        if not os.path.isdir(self.choose_dir):
+            self.warn_signal.emit()
+            return
+        for i, file in enumerate(os.listdir(self.choose_dir)):
+            if file.endswith(".json"):
+                self.trigger.emit({"prefix": "start", "line": f"{file}"})
+                # time.sleep(1)
+                file_name, ext = os.path.splitext(file)
+                chk_file_name = f"{file_name}.chk"
+                with open(os.path.join(self.choose_dir, file), "rb") as f:
+                    bs = f.read()  # read entire file as bytes
+                    chk_value = hashlib.sha256(bs).hexdigest()
+                with open(os.path.join(self.choose_dir, chk_file_name), 'w') as f:
+                    f.write(chk_value)
+                self.trigger.emit({"prefix": "end", "line": f"{file}"})
 
 
 def main():
+    cgitb.enable(format='text')
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
